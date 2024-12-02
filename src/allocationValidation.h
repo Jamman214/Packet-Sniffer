f4672bdf284b81ba3efd5fda068c9e72e949e2e7
Josh Turner <Josh.Turner.1@warwick.ac.uk>
2024-12-02 15:07:52 +0000

    Fixed header file

--- src/allocationValidation.h
+++ src/allocationValidation.h
@@ -1,5 +1,11 @@
+#ifndef CS241_ALLOCATION_VALIDATION_H
+#define CS241_ALLOCATION_VALIDATION_H
+
 #include <pthread.h>
 
 void validateAlloc(void* ptr, char* message);
 
 void validateAllocTS(pthread_mutex_t* print_lock, void* ptr, char* message);
+
+#endif
+
