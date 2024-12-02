a34b61cd80a94604bc55ab9de92f9f0054641503
Josh Turner <Josh.Turner.1@warwick.ac.uk>
2024-12-02 19:18:58 +0000

    Added comment

--- src/allocationValidation.c
+++ src/allocationValidation.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-
+// Prints error message and exits if memory allocation fails
 void validateAlloc(void* ptr, char* message) {
     if (!ptr) {
         fprintf(stderr, message);
